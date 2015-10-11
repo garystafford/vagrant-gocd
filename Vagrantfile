c4533573caa9b8fab8591cba4f3a0c481c7fdab0
Gary A. Stafford <garystafford@rochester.rr.com>
2015-10-10 21:14:07 -0400

    Final working scripts and Vagrantfile

--- Vagrantfile
+++ Vagrantfile
@@ -6,7 +6,9 @@ Vagrant.configure(2) do |config|
   config.vm.box = "hansode/fedora-21-server-x86_64"
   config.vm.hostname = "go-server"
   config.vm.network "forwarded_port", guest: 8153, host: 8153
-  config.vm.network "private_network", ip: "192.168.33.10", bridge: "eno1"
+  config.vm.network "forwarded_port", guest: 8154, host: 8154
+  #config.vm.network "public_network", ip: "192.168.1.122", bridge: "eno1"
+  config.vm.network "public_network", bridge: "eno1"
   config.vm.provision "shell", path: "bootstrap.sh"
   config.vm.provider :virtualbox do |vb|
  	vb.customize ["modifyvm", :id, "--name", "go-server"]
--- bootstrap_local_version.sh
+++ bootstrap_local_version.sh
@@ -1,11 +1,9 @@
 #! /usr/bin/env bash
 
-cd /vagrant
-
 # Install Oracle JDK
 file=jdk-8u60-linux-x64.rpm
 sudo rpm -ivh ${file}
-sed -i.bkp '/PATH=/i export JAVA_HOME=/usr/java/jdk1.8.0_60' /home/vagrant/.bash_profile
+sed -i.bkp '/PATH=/i export JAVA_HOME=/usr/java/jdk1.8.0_60' ~/.bash_profile
 javac -version
 
 # Install Gradle
@@ -15,8 +13,7 @@ sudo yum install -y unzip
 unzip ${file}
 sudo mkdir /usr/local/gradle/
 sudo mv gradle-${version} /usr/local/gradle/gradle-${version}
-sed -i.bkp '/PATH=/i export GRADLE_HOME=/usr/local/gradle/gradle-2.7' /home/vagrant/.bash_profile
-sed -i.bkp '/PATH=/i PATH=$PATH:$JAVA_HOME/bin:$GRADLE_HOME/bin' /home/vagrant/.bash_profile
+sed -i.bkp '/PATH=/i export GRADLE_HOME=/usr/local/gradle/gradle-2.7' ~/.bash_profile
 gradle --version
 
 # Install go-server and go-agent
@@ -28,22 +25,21 @@ sudo rpm -ivh go-agent-${version}.noarch.rpm
 file=gocd-gradle-plugin-1.0.0.jar
 plugin_path=/var/lib/go-server/plugins/external
 sudo cp ${file} ${plugin_path}/${file}
-sed -i.bkp '/PATH=/i export GO_SERVER=192.168.1.27' /home/vagrant/.bash_profile
+sed -i.bkp '/PATH=/i export GO_SERVER=192.168.1.76' ~/.bash_profile
 
-# firewalld vs. iptables
-sudo systemctl restart firewalld.service # make sure it is running
-sudo firewall-cmd --permanent --zone=public --add-port=22/tcp
-sudo firewall-cmd --permanent --zone=public --add-port=8153/tcp
-sudo firewall-cmd --permanent --zone=public --add-port=8154/tcp
-sudo systemctl restart firewalld.service
-sudo firewall-cmd --zone=public --list-all
+sed -i.bkp '/PATH=/i PATH=$PATH:$JAVA_HOME/bin:$GRADLE_HOME/bin' ~/.bash_profile
+
+#sudo gedit /etc/default/go-server # change 8153 and 8154
+#sudo gedit /etc/default/go-agent # change 8153 and 8154
 
 # firewalld vs. iptables
 #https://blog.christophersmart.com/2014/01/15/add-permanent-rules-to-firewalld/
-#sudo iptables -I INPUT 1 -p tcp --dport 8153 -j ACCEPT
-#sudo iptables -I INPUT 1 -p tcp --dport 8154 -j ACCEPT
-#sudo iptables -L -n | grep 815
-# cat /var/log/go-agent/go-agent.log # check for connection errors
+sudo systemctl restart firewalld.service # make sure it is running
+sudo firewall-cmd --permanent --zone=public --add-port=22/tcp
+sudo firewall-cmd --permanent --zone=public --add-port=8163/tcp
+sudo firewall-cmd --permanent --zone=public --add-port=8164/tcp
+sudo systemctl restart firewalld.service
+sudo firewall-cmd --zone=public --list-all
 
 sudo /etc/init.d/go-server restart && \
 sudo /etc/init.d/go-agent restart
